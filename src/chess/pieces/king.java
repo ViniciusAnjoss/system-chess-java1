2c23325b48298c402684fb553d277ed209e80416
ViniciusAnjoss <vinicius.ssilvaa@outlook.com>
2021-03-27 14:14:55 -0300

    add class king

--- src/chess/pieces/king.java
+++ src/chess/pieces/king.java
@@ -2,18 +2,18 @@ package chess.pieces;
 
 import boardgame.Board;
 import chess.ChessPiece;
-import chess.color;
+import chess.color; 
 
 
 
     public class king extends  ChessPiece {
 	
 	public king (Board board, color color) {
-		super (board, color);
+		super (board, color); 
 	}
 
 	public String toString() {
-		return "K";
+		return "K"; 
 	}
 
 	@Override
