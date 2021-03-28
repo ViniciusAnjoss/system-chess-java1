368386c79807334e379e1a41d39938b082081723
ViniciusAnjoss <vinicius.ssilvaa@outlook.com>
2021-03-28 19:46:15 -0300

    melhoria

--- src/application/program.java
+++ src/application/program.java
@@ -42,8 +42,11 @@ public class program {
 				
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
-					chessMatch.replacePromotedPiece(type);
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 				}
 			}
 			
--- src/chess/Chessmatch.java
+++ src/chess/Chessmatch.java
@@ -13,7 +13,7 @@ import chess.pieces.Tower;
 import chess.pieces.Bishop;
 import chess.pieces.Knight;
 import chess.pieces.Queen;
-import java.security.InvalidParameterException;
+
 
 
 
@@ -113,7 +113,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
